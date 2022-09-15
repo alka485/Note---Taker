43c59866ccf40c3f059868e613d4dc94227d1c8a
alkasah <alkasah485@gmail.com>
2022-09-15 00:31:05 -0700

    updation saved

--- server.js
+++ server.js
@@ -4,19 +4,34 @@ const fs = require('fs');
 const dbData = require('./db/db.json');
 
 const app = express();
-const port = 3001;
+const PORT = 3005;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.get('/notes' , function (req, res) {
-    res.sendFile(path.join(mainDir,"notes.html"));
+    res.sendFile(path.join(__dirname,'public/notes.html'));
 });
 
-app.get('/api/notes', function(req, res){
-    res.sendFile(path.join(__dirname, dbData));
+app.get('/' , function (req, res) {
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
+app.get('/api/notes',(req, res) => res.json(dbData));
+    
+
+app.post('/api/notes', (req, res)=>{
+    res.json(`${req.method} request received`);
+    console.info(req.rawHeaders);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received`);
+
+})
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
 
 
